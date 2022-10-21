da1af9510a7bc3611c397a10bf82c4791d7708b8
nzai <nzaih18@gmail.com>
2022-10-21 16:46:59 +0300

    finished responsiveness

--- src/main.js
+++ src/main.js
@@ -25,8 +25,6 @@ if(searchText){
      
     })
    })
-}else{
-  
 }
   
 })
--- src/ui.js
+++ src/ui.js
@@ -11,8 +11,8 @@ class UI {
         
         
      this.profile.innerHTML += `
-     <div class="card w-36 h-40 border-2 border-green-300 relative m-10">
-     <img src="${url+movie.poster_path}" class="w-full h-full " alt="">
+     <div class="card w-30 sm:w-36 m-8 h-40 border-2 border-green-300 relative sm:m-10">
+     <img loading="lazy"src="${url+movie.poster_path}" class="w-full h-full " alt="">
      <div class=" block items-center m-1">
        <h5 class=" inline-block  text-center  text-white text-sm m-2">${movie.title}</h5>
      <button class=" m-auto flex items-center justify-center bg-transparent p-1 outline-none rounded-lg text-white text-xl w-3 h-5"><i class=" fa-solid fa-arrow-right text-green-300 text-lg"></i></button>
@@ -25,7 +25,7 @@ class UI {
         let url = `https://image.tmdb.org/t/p/w500/`
         this.profile2.innerHTML += `
         <div class="card w-36 h-40 border-2 border-green-300 relative m-10">
-        <img src="${url+movie.poster_path}" class="w-full h-full " alt="">
+        <img loading="lazy" src="${url+movie.poster_path}" class="w-full h-full " alt="">
         <div class=" block items-center m-1">
           <h5 class=" inline-block  text-center  text-white text-sm m-2">${movie.title}</h5>
         <button class=" m-auto flex items-center justify-center bg-transparent p-1 outline-none rounded-lg text-white text-xl w-3 h-5"><i class=" fa-solid fa-arrow-right text-green-300 text-lg"></i></button>
@@ -38,8 +38,8 @@ class UI {
        
         let url = `https://image.tmdb.org/t/p/w500/`
         this.profile.innerHTML += `
-        <div class="card w-36 h-40 border-2 border-green-300 relative m-10">
-        <img src="${url+movie.poster_path}" class="w-full h-full " alt="">
+        <div class="block w-60 h-60 m-auto text-center card sm:w-36 sm:h-40 border-2 border-green-300 relative sm:m-10">
+        <img loading="lazy" src="${url+movie.poster_path}" class="w-full h-full " alt="">
         <div class=" block items-center m-1">
           <h5 class=" inline-block  text-center  text-white text-sm m-2">${movie.title}</h5>
         <button class=" m-auto flex items-center justify-center bg-transparent p-1 outline-none rounded-lg text-white text-xl w-3 h-5"><i class=" fa-solid fa-arrow-right text-green-300 text-lg"></i></button>
